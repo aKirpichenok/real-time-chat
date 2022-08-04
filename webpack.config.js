bc953b33a525fd81d5381353582ff4cf676d951e
aKirpichenok <a.kirpichenok99@gmail.com>
2022-08-04 23:30:38 +0300

    initialize project and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development'
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.tsx')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.[contenthash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(c | sa | sc)ss$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postCssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.m?js$/i,
+                exclude: /(node_modules | bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.bundle\.ts$/i,
+                use: {
+                    loader: 'bundle-loader',
+                    options: {
+                        name: '[name]'
+                    },
+                }
+            },
+            {
+                test: /\.tsx$/i,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', 'tsx', '.js']
+    }
+}
